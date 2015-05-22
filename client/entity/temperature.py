03e45542303531b5d060f11380b5cc69abb89ec1
flash286 <flash286@gmail.com>
2015-05-22 17:28:07 +0700

    Initial commit

---
+++ client/entity/temperature.py
@@ -0,0 +1,6 @@
+from client.entity.sensor import Sensor
+
+__author__ = 'nikolas'
+
+class TemperatureSensor(Sensor):
+    name = "temperature"
---
+++ client/transport/__init__.py
@@ -0,0 +1,12 @@
+__author__ = 'nikolas'
+
+
+class BlockChainTransport(object):
+    def __init__(self):
+        pass
+
+    def send_to_buffer(self, sensor_id, data, timestamp):
+        pass
+
+    def send_to_manager(self, sensor_id, timestamp, hash_id):
+        pass
