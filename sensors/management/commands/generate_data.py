d9247c871c11bca4b571303deebc2a43b673916f
flash286 <flash286@gmail.com>
2015-05-31 20:11:53 +0700

    fix for migrate from 2.7 to 3.3

--- sensors/management/commands/generate_data.py
+++ sensors/management/commands/generate_data.py
@@ -8,20 +8,17 @@ __author__ = 'nikolas'
 
 class Command(BaseCommand):
 
-
     def handle(self, *args, **options):
         data_range = lambda : random.randint(-30, 30)
         sensors = Sensor.objects.all()
         self.stdout.write("Available sensors: {}".format(sensors))
         start_time = datetime.datetime.utcnow()
         for sensor in sensors:
-
             last_record = SensorData.objects.last_record_or_false(sensor)
             if last_record:
-                print last_record.timestamp
+                print(last_record.timestamp)
                 start_time = last_record.timestamp + datetime.timedelta(seconds=1)
-
-            for interval in xrange(1, 100):
+            for interval in range(1, 100):
                 dt = start_time + datetime.timedelta(seconds=interval * 60)
                 value = data_range()
                 SensorData.objects.create(
